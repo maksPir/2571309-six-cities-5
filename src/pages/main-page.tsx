9e29eafd4f6a41d039450d699e061bb5557fcf3d
Pirushov Maxim <maximalka111@yandex.ru>
2024-11-13 18:37:36 +0400

    resolved conflict2

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -9,7 +9,7 @@ import { SortingPanel } from '../features/sorting-panel';
 
 export default function MainPage () {
   const {city, offers} = useAppSelector((state)=>state.offer);
-  const offersMockData = useMemo(()=>offers.filter((el)=>el.city.name === city), [city, offers]);
+  const offersFilteredData = useMemo(()=>offers.filter((el)=>el.city.name === city), [city, offers]);
   const [activeOffer, setActiveOffer] = useState<string>('');
   const onActiveOfferChangeCallback = (id: string) => {
     setActiveOffer(id);
@@ -65,40 +65,9 @@ export default function MainPage () {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-<<<<<<< HEAD
-              <b className="places__found">{offersMockData.length} places to stay in {city}</b>
-              <SortingPanel/>
-              <OffersList block='cities' offersMockData={offersMockData} onActiveOfferChangeCallback={onActiveOfferChangeCallback}/>
-=======
               <b className="places__found">{offersFilteredData.length} places to stay in {city}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <SortingPanel/>
               <OffersList block='cities' offersMockData={offersFilteredData} onActiveOfferChangeCallback={onActiveOfferChangeCallback}/>
->>>>>>> module6-task1
             </section>
             <div className="cities__right-section">
               <section className="cities__map map">
